2487d32512fffc88f967e37b67e3e0459b7f267c
MacHu-GWU <husanhe@gmail.com>
2016-06-07 02:47:15 -0400

    2016-06-07

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+def test_import():
+    from docfly import ApiReferenceDoc
+    from docfly import DocTree
+
 if __name__ == "__main__":
     import py
     py.test.cmdline.main("--tb=native -s")
