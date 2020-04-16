0306169de7a60fdc3eca8234581474e291fa400e
Sanhe Hu <husanhe@gmail.com>
2020-04-15 23:19:09 -0400

    bump version to 0.0.18

--- docfly/__init__.py
+++ docfly/__init__.py
@@ -7,7 +7,7 @@ docfly package.
 
 from __future__ import print_function
 
-__version__ = "0.0.17"
+__version__ = "0.0.18"
 __short_description__ = "A utility tool to help you build better sphinx documents."
 __license__ = "MIT"
 __author__ = "Sanhe Hu"
